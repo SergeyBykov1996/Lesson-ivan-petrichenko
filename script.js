31bd53320bf0bb7cc2e6f6ad723ea9a8dbb6296a
Sergey <bikova58@mail.ru>
2022-02-21 23:32:03 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,107 @@
+ "use strict";
+ 
+ //let number = 5;
+
+// console.log(number * "string");
+
+// let und;
+
+// console.log(und);
+
+// const box = {
+//     weight: 25,
+//     hight : 5
+// };
+
+// console.log(box.weight);
+
+// const person = ['box.png', 'heigt.png', 4, 1];
+
+// console.log(person[3]);
+
+
+
+
+
+// 2. Общение с пользователем.
+
+// alert('привет');    тревожное окно
+
+// const result = confirm("are you here ?");                 ОК или Отмена 
+
+// const answer = +prompt("Тебе есть 18?", "Введи сюда");
+// console.log(answer + 10);                                 Общение с пользователем на прямую
+
+
+// const answers = [];
+
+// answers[0] = prompt('Как вас зовут ? ');
+// answers[1] = prompt('какая ваша фамилия  ? ');
+// answers[2] = prompt('Сколько вам лет ? ');
+
+// console.log(answers);
+
+
+
+//   Урок 3. Интерполяция    как в React `${}`    Должно быть в бектиках 
+
+
+//   const category = 'toys';
+
+//   console.log(`https://someurl.com/${category}`);
+
+
+//  const nam = 'Sergey';
+//  const nam2 = 'Mary'
+
+//  alert(`ПРИВЕТ ${nam} и ${nam2}`);
+
+
+ 
+
+
+// 4 урок . Операторы 
+
+
+
+// && Оператор `и`
+
+// || Оператор  `или`` `что то из изловий правда, хотябы один. `
+
+// ! Оператор отрицания . Меняет на противоположный
+
+
+
+
+// console.log(2*6 === '12');
+
+// 1 пример:
+// const open = true,
+//       close = false;
+
+// console.log(open && close);     
+
+
+
+// 2 пример:
+
+// const open = false,
+//       close = false;
+
+// console.log(open || close); 
+
+
+
+
+
+// Инкримент и декримент.
+//  Префиксная и постфиксная форма. 
+// let incr = '10',
+//     decr = '10';
+
+// ++incr;
+// --decr;    
+
+// console.log(incr,decr);    
+
+
